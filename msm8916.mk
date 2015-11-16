a918af85e23f72d753ade8e73bb3b264ba9cf203
Jackeagle <jackeagle102@gmail.com>
2015-11-16 01:15:16 -0500

    Keylayout: Remove deprecated keylayout
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -144,7 +144,6 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
     $(LOCAL_PATH)/keylayout/Synaptics_RMI4_TouchPad_Sensor.idc:system/usr/idc/Synaptics_RMI4_TouchPad_Sensor.idc \
     $(LOCAL_PATH)/keylayout/Synaptics_HID_TouchPad.idc:system/usr/idc/Synaptics_HID_TouchPad.idc \
