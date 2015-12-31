516674b1a85c992d1039f129bdf154f0e85f105c
Jackeagle <jackeagle102@gmail.com>
2015-12-31 06:40:03 -0500

    msm8916-common: Add e5lte to build barrier
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- Android.mk
+++ Android.mk
@@ -18,7 +18,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8916)
-ifneq ($(filter e53g,$(TARGET_DEVICE)),) 
+ifneq ($(filter e53g e5lte,$(TARGET_DEVICE)),) 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
